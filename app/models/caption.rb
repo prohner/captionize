6de02c6dfb7656498105ddece7c408e2a6cb1075
Preston Rohner <prestonrohner@me.com>
2009-12-29 22:25:20 -0800

    Added counts of types of votes to captions

--- app/models/caption.rb
+++ app/models/caption.rb
@@ -2,4 +2,21 @@ class Caption < ActiveRecord::Base
   belongs_to :picture
   belongs_to :user
   has_many :votes
+
+  def count_of_votes_by_type(t)
+    vote_count = 0
+    votes.each do |v|
+      if v.is_up == t
+        vote_count += 1
+      end
+    end
+    vote_count
+  end
+
+  def count_of_up_votes
+    count_of_votes_by_type('y')
+  end
+  def count_of_down_votes
+    count_of_votes_by_type('n')
+  end
 end
