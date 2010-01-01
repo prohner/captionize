ab47402e4ecff2d385a7b950500c6e32d5242224
Preston Rohner <prestonrohner@me.com>
2009-12-31 22:13:47 -0800

    Setting up to deploy on ec2 on rails

--- db/migrate/20091230043017_create_pictures.rb
+++ db/migrate/20091230043017_create_pictures.rb
@@ -1,15 +1,17 @@
 class CreatePictures < ActiveRecord::Migration
   def self.up
+    drop_table :pictures
     create_table :pictures do |t|
       t.string :headline
       t.string :path_to_picture
       t.integer :user_id
+      t.binary :picture_data, :limit => 1.megabyte
+      t.string :content_type
+      
 
       t.timestamps
     end
 
-    add_column :pictures, :picture_data, :binary, :limit => 1.megabyte
-    add_column :pictures, :content_type, :string
   end
 
   def self.down
--- db/migrate/20091231042630_add_content_type_to_pictures.rb
+++
@@ -1,9 +0,0 @@
-class AddContentTypeToPictures < ActiveRecord::Migration
-  def self.up
-    add_column :pictures, :content_type, :string
-  end
-
-  def self.down
-    remove_column :pictures, :content_type
-  end
-end
