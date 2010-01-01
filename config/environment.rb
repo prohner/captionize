3382ca0f5cacabd0f4239e99387fdd6c987f8147
Preston Rohner <prestonrohner@me.com>
2009-12-31 20:49:01 -0800

    Setting up to deploy on ec2 on rails

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -38,4 +39,7 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  # Preston's own copied code
+  config.action_controller.session_store = :active_record_store 
 end
